9c42fd7a1d9171db3e488b30b88da5404580d589
Andrew McLachlan <amclachlan@4impact.com.au>
2018-09-06 18:17:33 +1000

    Modules <= 8.

---
+++ TAC/events.tac.local/Controllers/RelatedEventsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using events.tac.local.Business;
+
+namespace events.tac.local.Controllers
+{
+    public class RelatedEventsController : Controller
+    {
+        private readonly RelatedEventsProvider _builder;
+
+        public RelatedEventsController() : this(new RelatedEventsProvider()) { }
+
+        public RelatedEventsController(RelatedEventsProvider builder)
+        {
+            _builder = builder;
+        }
+
+        // GET: RelatedEvents
+        public ActionResult Index()
+        {
+            return View(_builder.GetRelatedEvents());
+        }
+    }
+}
